f80825d054880ce413ad192a6afc4dbc8e44fd00
routJB <112115350+routJB@users.noreply.github.com>
2024-01-24 14:23:39 -0500

    sgdfsdf

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.example._201532177_test_git {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.example._201532177_test_git to javafx.fxml;
+    exports org.example._201532177_test_git;
+}
